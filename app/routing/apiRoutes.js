28565bf2f7d35c54f859220c4d6ee9f776db9371
Andy Moore <moorejandy@gmail.com>
2020-01-16 18:22:51 -0500

    modal and updating routing files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,6 @@
+var path = require('path');
+
 var friends = require("../data/friends");
-
-
-
-
 
 
 
@@ -14,7 +12,7 @@ module.exports = function (app){
 
     app.post("/api/friends", function(req,res){
         var newFriend = req.body;
-        var scores = newFriend.scores;
+        var userScores = newFriend.scores;
         var bestMatch = {
             name: "",
             photo: "",
@@ -25,9 +23,27 @@ module.exports = function (app){
 //Loop through all friends in friends.js, nested loop to check for absolute differences, if total difference is less than equal to best
 //match.difference reset best match object to current friend 
 
+for (var i =0; i < friends.length; i++){
+    var totalDifference = 0;
+    var currentFriend = friends[i];
+    console.log(currentFriend.name);
 
+    for (var j = 0; j < currentFriend.scores.length; j++) {
+        currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
 
-//res.json(bestMatch);
+        totalDifference += Math.abs(currentUserScore - currentFriendScore);
+    }
+
+    if (totalDifference <= bestMatch.difference){
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.difference = totalDifference;
+
+    }
+}
+
+res.json(bestMatch);
 
     })
 
